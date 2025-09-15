4d0ae61c2e00a1f8ca2acf9fe2747cda9ac94cf3
TanuAtSoft <tanuja_gurung@softprodigy.com>
2025-09-15 18:44:52 +0530

    worked on fixes

--- comps/Marquee.jsx
+++ comps/Marquee.jsx
@@ -1,49 +1,26 @@
 "use client";
 
-import { memo } from "react";
+import styles from "../styles/Marquee.module.css";
 import Products from "../app/Products";
-
-// Swiper imports
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "../styles/Marquee.module.css"
-
-export default function Marquee({ products }) {
+import { memo } from "react";
+ 
+const Marquee = ({ products }) => {
+  console.log("marquee");
   return (
-    <div className="mt-20">
-      <h1 className="text-center text-secondary text-xl font-extrabold">
+    <div className=" mt-20">
+      <h1 className=" text-center text-secondary text-xl font-extrabold">
         You may also like
       </h1>
-
-      <section className="mt-10 relative w-full">
-        {/* Give the Swiper a custom class so CSS targets only this slider */}
-        <Swiper
-          modules={[Navigation, Pagination]}
-          spaceBetween={20}
-          slidesPerView={1}
-          autoplay={true}
-          navigation
-          pagination={{ clickable: true }}
-          breakpoints={{
-            640: { slidesPerView: 2 },
-            1024: { slidesPerView: 4 },
-          }}
-          className="marqueeSwiper"
-        >
+ 
+      <section className=" mt-10 relative h-52 sm:h-96  w-full  overflow-hidden">
+        <div className={`flex  justify-center ${styles.anim}`}>
           {products.map((product) => (
-            <SwiperSlide key={product._id}>
-              <div className="px-2">
-                <Products gap={"mr-5"} products={product} />
-              </div>
-            </SwiperSlide>
+            <Products gap={"mr-5"} key={product._id} products={product} />
           ))}
-        </Swiper>
+        </div>
       </section>
     </div>
   );
-}
-
-export const MemoizedMarquee = memo(Marquee);
+};
+ 
+export default memo(Marquee);
